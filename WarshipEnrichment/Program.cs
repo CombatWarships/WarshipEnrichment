432190ba1fb984fad10e762bf21cc82f4c547612
Justin Scott <justin@scott.org>
2023-11-26 09:03:19 -0600

    Removed Serilog.ASP so the app would run.

--- WarshipEnrichment/Program.cs
+++ WarshipEnrichment/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Sinks.ApplicationInsights.TelemetryConverters;
 using ServiceBus.Core;
-using System.Net.NetworkInformation;
 using WarshipEnrichment;
 using WarshipEnrichment.Converters;
 using WarshipEnrichment.DataCollectors;
@@ -15,9 +14,6 @@ using WarshipImport.Managers;
 using WarshipRegistryAPI.Classification;
 using WarshipRegistryAPI.Nationality;
 using WarshipRegistryAPI.Warships;
-
-await Task.Delay(5000);
-
 
 
 var appInsightsConnection = "InstrumentationKey=09ce9924-198e-4315-b6c8-7885f28ec8e9;IngestionEndpoint=https://southcentralus-3.in.applicationinsights.azure.com/;LiveEndpoint=https://southcentralus.livediagnostics.monitor.azure.com/";
